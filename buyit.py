a2d403ce8d67c66088a0c2627a7830ef8aa4e6ce
dglv <ddem0025@gmail.com>
2020-05-10 16:08:59 +0300

    BI-8 Major changes on server side to support multiple orders. Reworked ajax requests to send order_id parameter

--- buyit.py
+++ buyit.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 @app.route('/main', methods=['GET'])
 def render_main_page():
-    orders_lengths = [15, 0, 10, 0, 0, 0, 8, 11]
+    orders_lengths = service.get_orders_lengths()
     return render_template('main.html',
                            the_title='Выберите корзину',
                            the_orders_lengths=orders_lengths)
@@ -20,13 +20,13 @@ def render_item_page():
     return render_template('item.html',
                            the_title='Создание Заказа',
                            the_order_id=order_id,
-                           the_order_length=len(service.get_order()))
+                           the_order_length=len(service.get_order(order_id)))
 
 
 @app.route('/order', methods=['GET'])
 def render_order_page():
     order_id = request.args.get('order_id')
-    order = service.get_order()
+    order = service.get_order(order_id)
     return render_template('order.html',
                            the_order_id=order_id,
                            the_title='Выполнение Заказа',
@@ -35,30 +35,33 @@ def render_order_page():
 
 @app.route('/item', methods=['POST'])
 def update_item():
+    order_id = request.form['order_id']
     item = request.form['item']
     comment = request.form['comment']
     is_checked = request.form['is_checked']
     if item:
-        service.update_item(item, comment, is_checked)
-    return json.dumps({'success': True, 'order_length': len(service.get_order())}), 200, {
+        service.update_item(order_id, item, comment, is_checked)
+    return json.dumps({'success': True, 'order_length': len(service.get_order(order_id))}), 200, {
         'ContentType': 'application/json'}
 
 
 @app.route('/item', methods=['DELETE'])
 def remove_item():
+    order_id = request.form['order_id']
     item = request.form['item']
     if item:
-        service.remove_item(item)
+        service.remove_item(order_id, item)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/items', methods=['DELETE'])
 def remove_items():
+    order_id = request.form['order_id']
     items_str = request.form['items']
     if items_str:
         items = json.loads(items_str)
         for i in items:
-            service.remove_item(i)
+            service.remove_item(order_id, i)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
@@ -78,7 +81,8 @@ def render_release_notes():
 
 @app.route('/order', methods=['DELETE'])
 def remove_order():
-    service.remove_order()
+    order_id = request.form['order_id']
+    service.remove_order(order_id)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
--- service.py
+++ service.py
@@ -1,38 +1,60 @@
 import os.path
 import filehandler
 
-ORDER_FILE = 'order.txt'
+ORDER_TEMPLATE = 'order_{}.txt'
 RELEASE_NOTES_FILE = 'release_notes.txt'
 
-if os.path.isfile(ORDER_FILE):
-    _cache = filehandler.read_as_json(ORDER_FILE)
-else:
-    _cache = {}
+
+def get_order_filename(order_id: int) -> str:
+    return ORDER_TEMPLATE.format(order_id)
 
 
-def update_item(item, comment, is_checked) -> None:
-    _cache[item] = {'comment': comment, 'is_checked': is_checked}
-    filehandler.write_as_json(ORDER_FILE, _cache)
+def update_item(order_id: int, item: str, comment: str, is_checked: str) -> None:
+    order_filename = get_order_filename(order_id)
+    _cache[order_filename][item] = {'comment': comment, 'is_checked': is_checked}
+    filehandler.write_as_json(order_filename, _cache[order_filename])
 
 
-def get_item(item) -> dict:
-    return {item: _cache[item]}
+def get_item(order_id: int, item: str) -> dict:
+    order_filename = get_order_filename(order_id)
+    return {item: _cache[order_filename][item]}
 
 
-def remove_item(item) -> None:
-    del _cache[item]
-    filehandler.write_as_json(ORDER_FILE, _cache)
+def remove_item(order_id: int, item: str) -> None:
+    order_filename = get_order_filename(order_id)
+    del _cache[order_filename][item]
+    filehandler.write_as_json(order_filename, _cache[order_filename])
 
 
-def get_order() -> dict:
-    return _cache
+def get_order(order_id: int) -> dict:
+    order_filename = get_order_filename(order_id)
+    return _cache[order_filename]
 
 
-def remove_order() -> None:
-    _cache.clear()
-    filehandler.write_as_json(ORDER_FILE, _cache)
+def remove_order(order_id: int) -> None:
+    order_filename = get_order_filename(order_id)
+    _cache[order_filename] = {}
+    filehandler.write_as_json(order_filename, _cache[order_filename])
 
 
 def get_release_notes() -> str:
     data = filehandler.read_plain(RELEASE_NOTES_FILE)
-    return data;
+    return data
+
+
+def get_orders_lengths() -> list:
+    orders_lengths = []
+    for order in _cache.values():
+        orders_lengths.append(len(order))
+    return orders_lengths
+
+
+_cache = {}
+for i in range(8):
+    _order_id = i + 1
+    _order_filename = get_order_filename(_order_id)
+    if os.path.isfile(_order_filename):
+        _cache[_order_filename] = filehandler.read_as_json(_order_filename)
+    else:
+        _cache[_order_filename] = {}
+        filehandler.write_as_json(_order_filename, _cache[_order_filename])
